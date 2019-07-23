f46c4d6c1dad98d469db2e5cd3ab7a88e5aef88e
hygoni <9hrack@naver.com>
2019-07-23 05:48:34 +0000

    fixed flask bug

--- Web/web.py
+++ Web/web.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-def alert(message):
-
-
-@app.route('/subscribeFrom'):
+@app.route('/subscribeFrom')
 def mainPage():
 	return render_template('./subscribe.html')
 
