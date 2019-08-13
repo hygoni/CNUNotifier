c6c8a9a17a3ee77374129a9ddd3989aea7a16f10
hygoni <9hrack@naver.com>
2019-08-13 12:08:44 +0000

    removed test file

--- my.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-conn = pymysql.connect(host='localhost', user='cnunoti', password='localhost', db='cnunoti')
-cursor = conn.cursor()
-cursor.execute('SELECT count(*) FROM cse')
-result = cursor.fetchone()[0]
-print(result)
-
